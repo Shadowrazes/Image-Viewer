88ad62b7f2c2b91743fd3a56bc84741a2dbe5729
Михаил <hugehug@mail.ru>
2022-04-10 17:16:44 +0700

    Change load of files

--- Image Viewer/Models/Node.cs
+++ Image Viewer/Models/Node.cs
@@ -10,15 +10,63 @@ namespace Image_Viewer.Models
 {
     public class Node
     {
+        private bool isHashed;
         public ObservableCollection<Node>? FilesAndFolders { get; set; }
 
         public string NodeName { get; }
         public string FullPath { get; }
 
-        public Node(string _FullPath)
+        public Node(string _FullPath, bool isDisk)
         {
+            FilesAndFolders = new ObservableCollection<Node>();
             FullPath = _FullPath;
-            NodeName = Path.GetFileName(_FullPath);
+            if (!isDisk)
+                NodeName = Path.GetFileName(_FullPath);
+            else
+                NodeName = "Диск " + _FullPath.Substring(0, _FullPath.IndexOf(":"));
+            isHashed = false;
+        }
+
+        public void LoadNext()
+        {
+            foreach(Node file in FilesAndFolders)
+            {
+                if (!file.isHashed)
+                {
+                    file.GetFilesAndFolders();
+                }
+            }
+        }
+
+        public void GetFilesAndFolders()
+        {
+            if (!isHashed)
+            {
+                try
+                {
+                    IEnumerable<string> subdirs = Directory.EnumerateDirectories(FullPath, "*", SearchOption.TopDirectoryOnly);
+                    foreach (string dir in subdirs)
+                    {
+                        Node thisnode = new Node(dir, false);
+                        FilesAndFolders.Add(thisnode);
+                    }
+
+                    string[] allowedExtensions = new[] { ".jpg", ".jpeg", ".png" };
+                    IEnumerable<string> files = Directory.EnumerateFiles(FullPath)
+                        .Where(file => allowedExtensions.Any(file.ToLower().EndsWith))
+                        .ToList();
+
+                    foreach (string file in files)
+                    {
+                        FilesAndFolders.Add(new Node(file, false));
+                    }
+                }
+                catch
+                {
+
+                }
+                isHashed = true;
+            }
         }
     }
 }
--- Image Viewer/Views/MainWindow.axaml.cs
+++ Image Viewer/Views/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Image_Viewer.Models;
+using Avalonia.Interactivity;
 
 namespace Image_Viewer.Views
 {
@@ -7,6 +9,12 @@ namespace Image_Viewer.Views
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void SelectedNodeChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Node selectedNode = e.AddedItems[0] as Node;
+            selectedNode.LoadNext();
         }
     }
 }
